645d7fe80a629e8f488aa34d14231c6385cedc07
Ralph Schaer <ralphschaer@gmail.com>
2012-11-07 18:02:07 +0100

    Fix problem with index creater and reader because they run parallel. Now the program first creates the index and then opens it for reads in sequential order.

--- src/main/java/ch/rasc/musicsearch/service/IndexService.java
+++ src/main/java/ch/rasc/musicsearch/service/IndexService.java
@@ -1,22 +1,34 @@
 package ch.rasc.musicsearch.service;
 
+import java.io.FileWriter;
 import java.io.IOException;
+import java.nio.file.Files;
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.Properties;
+import java.util.concurrent.TimeUnit;
 
 import javax.annotation.PostConstruct;
 import javax.annotation.PreDestroy;
 
 import org.apache.commons.logging.Log;
 import org.apache.commons.logging.LogFactory;
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.index.DirectoryReader;
 import org.apache.lucene.index.IndexReader;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexWriterConfig.OpenMode;
 import org.apache.lucene.search.IndexSearcher;
 import org.apache.lucene.store.Directory;
 import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.env.Environment;
 import org.springframework.stereotype.Service;
+
+import com.google.common.base.Stopwatch;
 
 @Service
 public class IndexService {
@@ -33,15 +45,17 @@ public class IndexService {
 	private Environment environement;
 
 	@PostConstruct
-	public void init() {
+	public void init() throws IOException {
+
 		Path ixDir = Paths.get(environement.getProperty("indexDir"));
-		try {
-			indexDirectory = FSDirectory.open(ixDir.toFile());
-			indexReader = DirectoryReader.open(indexDirectory);
-			indexSearcher = new IndexSearcher(indexReader);
-		} catch (IOException e) {
-			logger.error("init searcher service", e);
+
+		if (!Files.exists(ixDir)) {
+			indexMusic(ixDir);
 		}
+
+		indexDirectory = FSDirectory.open(ixDir.toFile());
+		indexReader = DirectoryReader.open(indexDirectory);
+		indexSearcher = new IndexSearcher(indexReader);
 	}
 
 	@PreDestroy
@@ -64,6 +78,41 @@ public class IndexService {
 
 	public IndexSearcher getIndexSearcher() {
 		return indexSearcher;
+	}
+
+	private void indexMusic(Path ixDir) throws IOException {
+
+		Path musicDir = Paths.get(environement.getProperty("musicDir"));
+
+		try (Directory dir = FSDirectory.open(ixDir.toFile());
+				Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_40)) {
+			IndexWriterConfig iwc = new IndexWriterConfig(Version.LUCENE_40, analyzer);
+			// iwc.setOpenMode(OpenMode.CREATE);
+			iwc.setOpenMode(OpenMode.CREATE_OR_APPEND);
+
+			Stopwatch stopwatch = new Stopwatch();
+			stopwatch.start();
+
+			IndexFileWalker walker = null;
+
+			try (IndexWriter writer = new IndexWriter(dir, iwc)) {
+				walker = new IndexFileWalker(writer, musicDir);
+				Files.walkFileTree(musicDir, walker);
+
+				writer.forceMerge(1);
+			}
+
+			stopwatch.stop();
+			logger.info("INDEXING TIME: " + stopwatch.elapsedTime(TimeUnit.SECONDS));
+
+			Path infoFile = ixDir.resolve("info.properties");
+			try (FileWriter fw = new FileWriter(infoFile.toFile())) {
+				Properties properties = new Properties();
+				properties.put("totalDuration", String.valueOf(walker.getTotalDuration()));
+				properties.put("noOfSongs", String.valueOf(walker.getNoOfSongs()));
+				properties.store(fw, "dbinfo");
+			}
+		}
 	}
 
 }
