03bd44c5e995b4da1f65b06ff4300094484cb19f
Ralph Schaer <ralphschaer@gmail.com>
2012-11-29 07:22:27 +0100

    Remove unused import statement

--- src/main/java/ch/rasc/musicsearch/config/SpringConfig.java
+++ src/main/java/ch/rasc/musicsearch/config/SpringConfig.java
@@ -5,8 +5,6 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
-import org.springframework.web.multipart.MultipartResolver;
-import org.springframework.web.multipart.commons.CommonsMultipartResolver;
 import org.springframework.web.servlet.config.annotation.DefaultServletHandlerConfigurer;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.config.annotation.ViewControllerRegistry;
