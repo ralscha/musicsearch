386cdffeea05343d4d0c07d86e170cc5d832bb7f
Ralph Schaer <ralphschaer@gmail.com>
2013-07-21 12:17:05 +0200

    Write two zeros when 0 minutes. Looks nicer

--- src/main/webapp/app.js
+++ src/main/webapp/app.js
@@ -22,7 +22,7 @@ MusicSearch.Utils.secondsToHms = function(d) {
 	var m = Math.floor(no % 3600 / 60);
 	var s = Math.floor(no % 3600 % 60);
 	return ((h > 0 ? h + ":" : "")
-			+ (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "0:")
+			+ (m > 0 ? (h > 0 && m < 10 ? "0" : "") + m + ":" : "00:")
 			+ (s < 10 ? "0" : "") + s);
 };
 
@@ -30,7 +30,6 @@ soundManager.setup({
 	url: app_context_path + '/resources/swf/',
 	flashVersion: 9,
 	preferFlash: true,
-	debugMode: true,
 	onready: function() {
 		Ext.create('MusicSearch.App');
 	}
