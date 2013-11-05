07326e1fb58a8047a39cc2365db4b931e934cc25
Ralph Schaer <ralphschaer@gmail.com>
2013-11-05 04:32:22 +0100

    Move switch for production/development profile into starter class

--- src/main/java/StartMusicSearch.java
+++ src/main/java/StartMusicSearch.java
@@ -2,6 +2,9 @@ import ch.rasc.embeddedtc.EmbeddedTomcat;
 
 public class StartMusicSearch {
 	public static void main(String[] args) throws Exception {
+		// Comment out the following line to activate production profile
+		System.setProperty("spring.profiles.active", "development");
+
 		EmbeddedTomcat.create().useNio().setContextFile("./src/main/config/tomcat.xml").startAndWait();
 	}
 }
