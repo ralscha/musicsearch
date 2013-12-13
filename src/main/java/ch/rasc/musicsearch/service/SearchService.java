b9a8297f5d2c556b24362526e9993e8ebd469fa4
Ralph Schaer <ralphschaer@gmail.com>
2013-12-13 09:48:59 +0100

    Use lucene 4.6

--- src/main/java/ch/rasc/musicsearch/service/SearchService.java
+++ src/main/java/ch/rasc/musicsearch/service/SearchService.java
@@ -106,9 +106,9 @@ public class SearchService {
 
 		List<Song> resultList = new ArrayList<>();
 
-		try (Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_45)) {
+		try (Analyzer analyzer = new StandardAnalyzer(Version.LUCENE_46)) {
 
-			MultiFieldQueryParser parser = new MultiFieldQueryParser(Version.LUCENE_45, FIELDS, analyzer);
+			MultiFieldQueryParser parser = new MultiFieldQueryParser(Version.LUCENE_46, FIELDS, analyzer);
 			parser.setDefaultOperator(QueryParserBase.AND_OPERATOR);
 			parser.setAllowLeadingWildcard(true);
 
